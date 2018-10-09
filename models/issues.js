789de5c45c9669fca59adc15120f7c5184f8e950
zhusheng <1013702057@qq.com>
2018-10-09 18:37:14 +0800

    Issue model schema refactory

--- models/issues.js
+++ models/issues.js
@@ -1,7 +1,10 @@
-const issues = [
-    {id: 1000000, category: 'Business', status: true, solution: 3, solutionList:[1000000,1000001,1000003]},
-    {id: 1000001, category: 'Academy', status: false, solution: 1,solutionList:[1000002]},
-    {id: 1000002, category: 'Life_trifles', status: false, solution: 0,solutionList:[]}
-];
+let mongoose = require('mongoose');
 
-module.exports = issues;
+let IssueSchema = new mongoose.Schema({
+        category: String,
+        status: Boolean ,
+        solutions: []
+    },
+    { collection: 'issuesdb' });
+
+module.exports = mongoose.model('Issue', IssueSchema);
